384d7673887427c5ab5fc79a7857a00ede075312
ferly kurniawan <89645813+FerlyKurniawan@users.noreply.github.com>
2026-01-20 19:42:00 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv, find_dotenv
 # Loading .env variables
 load_dotenv(find_dotenv())
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_TOKEN = os.getenv("8349591511:AAFcoR2s2ue0wRqd4nS4ccWpF3xKKxoB7d8")
 if TELEGRAM_TOKEN is None:
     raise Exception("Please setup the .env variable TELEGRAM_TOKEN.")
 
 PORT = int(os.environ.get("PORT", "8443"))
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
 
-TELEGRAM_SUPPORT_CHAT_ID = os.getenv("TELEGRAM_SUPPORT_CHAT_ID")
+TELEGRAM_SUPPORT_CHAT_ID = os.getenv("8038986965 347422710")
 if (
     TELEGRAM_SUPPORT_CHAT_ID is None
     or not str(TELEGRAM_SUPPORT_CHAT_ID).lstrip("-").isdigit()
